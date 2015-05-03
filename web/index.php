433fbfca13908c9571d018a5102669bc8c2dfb22
Mike Funk <mike@mikefunk.com>
2015-05-03 08:23:51 -0700

    added getenv for TIMES

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@ $app->register(
 
 $app->get('/', function () use ($app) {
     $app['monolog']->addDebug('logging output.');
-    return 'Hello';
+    return str_repeat('Hello', getenv('TIMES'));
 });
 
 $app->get('/twig/{name}', function ($name) use ($app) {
