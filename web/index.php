053d08fd9efc592e065a02f1bcab34b6ddd51dee
Mike Funk <mike@mikefunk.com>
2015-05-03 09:27:16 -0700

    database test

--- web/index.php
+++ web/index.php
@@ -21,6 +21,18 @@ $app->register(
     )
 );
 
+// and the db
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(
+    new PdoServiceProvider(),
+    array(
+        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"], '/').';host='.$dbopts["host"],
+        'pdo.username' => $dbopts['user'],
+        'pdo.password' => $dbopts['pass'],
+        'pdo.port' => $dbopts['port'],
+    )
+);
+
 // Our web handlers
 
 $app->get('/', function () use ($app) {
@@ -32,6 +44,18 @@ $app->get('/twig/{name}', function ($name) use ($app) {
     return $app['twig']->render('index.twig', array(
         'name' => $name,
     ));
+});
+
+$app->get('/db/', function () use ($app) {
+    $statement = $app['pdo']->prepare('SELECT name FROM test_table');
+    $statement->execute();
+
+    $names = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
+    return $app['twig']->render('database.twig', compact('names'));
 });
 
 $app->run();
